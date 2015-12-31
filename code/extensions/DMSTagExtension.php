a5fce188e0aefb7f02e703fe678cee53eb751a8c
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:06:17 +1300

    Converted to PSR-2

--- code/extensions/DMSTagExtension.php
+++ code/extensions/DMSTagExtension.php
@@ -1,29 +1,37 @@
 <?php
 
-class DMSTagExtension extends DataExtension {
-	
-	public static $undefinedValue = '#undefinedValue#';
+class DMSTagExtension extends DataExtension
+{
+    
+    public static $undefinedValue = '#undefinedValue#';
 
-	public function isValueDefined() {
-		if($this->owner->Value != self::$undefinedValue) return true;
-		return false;
-	}
+    public function isValueDefined()
+    {
+        if ($this->owner->Value != self::$undefinedValue) {
+            return true;
+        }
+        return false;
+    }
 
-	public function validate(ValidationResult $validationResult) {
-		if(!$this->owner->Category)
-			$validationResult->combineAnd(new ValidationResult(false, _t('DMSTagExtension.NoCategory', 'You must at least enter a category.')));
-		else {
-			$this->owner->Category = trim($this->owner->Category);
-			
-			// this is done to prevent the method 'removeTag' in 'DMSDocument' to erase all the tags with same category if no value is defined on the removing Tag
-			if(!$this->owner->Value) $this->owner->Value = self::$undefinedValue;
-			else $this->owner->Value = trim($this->owner->Value);
-			
-			$query = "SELECT COUNT(*) FROM \"DMSTag\" WHERE \"DMSTag\".\"Category\" LIKE '{$this->owner->Category}' AND \"DMSTag\".\"Value\" LIKE '{$this->owner->Value}'";
+    public function validate(ValidationResult $validationResult)
+    {
+        if (!$this->owner->Category) {
+            $validationResult->combineAnd(new ValidationResult(false, _t('DMSTagExtension.NoCategory', 'You must at least enter a category.')));
+        } else {
+            $this->owner->Category = trim($this->owner->Category);
+            
+            // this is done to prevent the method 'removeTag' in 'DMSDocument' to erase all the tags with same category if no value is defined on the removing Tag
+            if (!$this->owner->Value) {
+                $this->owner->Value = self::$undefinedValue;
+            } else {
+                $this->owner->Value = trim($this->owner->Value);
+            }
+            
+            $query = "SELECT COUNT(*) FROM \"DMSTag\" WHERE \"DMSTag\".\"Category\" LIKE '{$this->owner->Category}' AND \"DMSTag\".\"Value\" LIKE '{$this->owner->Value}'";
 
-			if(DB::query($query)->value()) 
-				$validationResult->combineAnd(new ValidationResult(false, _t('DMSTagExtension.CategoryValueNotUnique', 'This tag already exists. Please select it from the multiselect box.')));
-
-		}
-	}
+            if (DB::query($query)->value()) {
+                $validationResult->combineAnd(new ValidationResult(false, _t('DMSTagExtension.CategoryValueNotUnique', 'This tag already exists. Please select it from the multiselect box.')));
+            }
+        }
+    }
 }
